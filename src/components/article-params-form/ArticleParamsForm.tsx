c90935a53762bf772d73afc2ec03eb376efc3a46
Kseniya Dereka <mail@kseniyadereka.com>
2024-04-17 15:41:28 +0300

    feat: добавила заголовок в форму

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,5 +1,6 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from 'components/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
@@ -9,6 +10,9 @@ export const ArticleParamsForm = () => {
 			<ArrowButton />
 			<aside className={styles.container}>
 				<form className={styles.form}>
+				<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+				Задайте параметры
+			</Text>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
