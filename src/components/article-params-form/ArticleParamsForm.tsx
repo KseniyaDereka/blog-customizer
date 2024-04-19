2c81e31a483623ad0780e935931cc2d121f5f7f2
Kseniya Dereka <mail@kseniyadereka.com>
2024-04-19 16:41:28 +0300

    feat: отобразить компонент выбора размера шрифта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,6 +1,10 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 import { Text } from 'components/text';
+import { Select } from 'components/select/Select'
+import { ReactNode } from 'react';
+import { RadioGroup } from 'components/radio-group';
+import { defaultArticleState, fontSizeOptions, AppState } from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 import { useLayoutEffect, useState } from 'react';
@@ -8,14 +12,18 @@ import { useLayoutEffect, useState } from 'react';
 type ArticleParamsProps = {
 	title: string,
 	active: boolean,
-	setIsOpen: any
+	setIsOpen: (callback: (previous: boolean) => boolean) => void,
+	// children: ReactNode;
+	setAppState: any
+	appState: AppState
 }
 //индикатор видимости, и функцию открыть/закрыть
 
 
-export const ArticleParamsForm = ({ title, active, setIsOpen }: ArticleParamsProps) => {
+export const ArticleParamsForm = ({ title, active, setIsOpen, setAppState, appState }: ArticleParamsProps) => {
 
 	const className = styles.container + (active ? ' ' + styles.container_open : '');
+
 
 	return (
 		<>
@@ -25,7 +33,9 @@ export const ArticleParamsForm = ({ title, active, setIsOpen }: ArticleParamsPro
 					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
 						{title}
 					</Text>
+					<RadioGroup title={'Шрифт'} options={fontSizeOptions} name={'fontSize'} selected={appState.fontSizeOption} onChange={(fontSizeOption) => setAppState({ ...appState, fontSizeOption })} />
 					<div className={styles.bottomContainer}>
+						{/* {children} */}
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, OptionType } from './constants/articleProps';
+
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -16,19 +17,21 @@ const root = createRoot(domNode);
 
 const App = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [appState, setAppState] = useState(defaultArticleState);
+	// const [appState, setAppState] = useSearchParams(defaultArticleState);
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appState.fontFamilyOption.value,
+					'--font-size': appState.fontSizeOption.value,
+					'--font-color': appState.fontColor.value,
+					'--container-width': appState.contentWidth.value,
+					'--bg-color': appState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm title={'Задайте параметры'} active={isOpen} setIsOpen={setIsOpen} />
+			<ArticleParamsForm title={'Задайте параметры'} active={isOpen} setIsOpen={setIsOpen} setAppState={setAppState} appState={appState} />
 			<Article />
 		</div>
 	);
